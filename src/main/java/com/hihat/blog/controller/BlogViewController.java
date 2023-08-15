069c05d22cf862789e6e744ba0db2cce77464d41
BeomSeok Yu <beomseok.dev@gmail.com>
2023-08-15 16:09:04 +0900

    add articleList.html / BlogViewController.java / dto

---
+++ src/main/java/com/hihat/blog/controller/BlogViewController.java
@@ -0,0 +1,27 @@
+package com.hihat.blog.controller;
+
+import com.hihat.blog.dto.ArticleListViewResponse;
+import com.hihat.blog.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+        return "articleList";
+    }
+}
