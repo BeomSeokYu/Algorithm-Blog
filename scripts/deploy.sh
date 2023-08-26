6a4d0b2b3dc69a69de406e8bb0621e1456b24d8b
BeomSeok Yu <beomseok.dev@gmail.com>
2023-08-26 23:20:07 +0900

    shell script 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 BUILD_JAR=$(ls /home/ubuntu/app/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/app/deploy.log
+DEPLOY_LOG=/home/ubuntu/app/deploy.log
+echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG
 
-echo "> build 파일 복사" >> /home/ubuntu/app/deploy.log
-DEPLOY_PATH=/home/ubuntu/app/
-cp $BUILD_JAR $DEPLOY_PATH
-
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/app/deploy.log
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> $DEPLOY_LOG
 CURRENT_PID=$(pgrep -f $JAR_NAME)
+echo "  > : $CURRENT_PID" >> $DEPLOY_LOG
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/app/deploy.log
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> $DEPLOY_LOG
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/app/deploy.log
-nohup java -jar -Dserver.port=8080 $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/app/deploy_err.log &
+echo "> DEPLOY_JAR 배포"    >> $DEPLOY_LOG
+nohup java -jar $BUILD_JAR >> /home/ubuntu/tomcat_exe.log 2>/home/ubuntu/app/deploy_err.log &
