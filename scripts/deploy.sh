be1cffbf8a108bafcc05c5f97529187ba6f42953
BeomSeok Yu <beomseok.dev@gmail.com>
2023-08-27 18:24:29 +0900

    CD 관련 설정 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ubuntu/app/build/libs/*.jar)
+BUILD_JAR=$(ls /home/ubuntu/app-blog/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-DEPLOY_LOG=/home/ubuntu/app/deploy.log
-echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG
+PROJECT_DIR=/home/ubuntu/app-blog
+DEPLOY_LOG=$PROJECT_DIR/log/deploy.log
+DEPLOY_ERR_LOG=$PROJECT_DIR/log/deploy_err.log
+EXEC_LOG=$PROJECT_DIR/log/exec.log
+echo "> build 파일명: $JAR_NAME" > $DEPLOY_LOG
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> $DEPLOY_LOG
 CURRENT_PID=$(pgrep -f $JAR_NAME)
-echo "  > : $CURRENT_PID" >> $DEPLOY_LOG
 
 if [ -z $CURRENT_PID ]
 then
@@ -17,5 +19,5 @@ else
   sleep 5
 fi
 
-echo "> DEPLOY_JAR 배포"    >> $DEPLOY_LOG
-nohup java -jar $BUILD_JAR >> /home/ubuntu/tomcat_exe.log 2>/home/ubuntu/app/deploy_err.log &
+echo "> DEPLOY_JAR 배포 (exec.log에 스프링 실행 로그가 없다면 deploy_err.log을 확인하세요)" >> $DEPLOY_LOG
+nohup java -jar $BUILD_JAR >> $EXEC_LOG 2> $DEPLOY_ERR_LOG &
