fd6d3df520576f20e253038c6c9c16975fc0b4a9
BeomSeokYu <beomseok.dev@gmail.com>
2026-01-01 12:51:54 +0900

    Restrict OAuth users and update Google button

--- src/main/java/com/hihat/blog/config/oauth/OAuth2UserCustomService.java
+++ src/main/java/com/hihat/blog/config/oauth/OAuth2UserCustomService.java
@@ -5,6 +5,7 @@ import com.hihat.blog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
@@ -19,20 +20,23 @@ public class OAuth2UserCustomService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User user = super.loadUser(userRequest);
-        saveOrUpdate(user);
+        ensureExistingUser(user);
         return user;
     }
 
-    public void saveOrUpdate(OAuth2User oAuth2User) {
+    private void ensureExistingUser(OAuth2User oAuth2User) {
         Map<String, Object> attributes = oAuth2User.getAttributes();
         String email = (String) attributes.get("email");
         String name = (String) attributes.get("name");
+        if (email == null || email.isBlank()) {
+            throw new OAuth2AuthenticationException(new OAuth2Error("email_not_found", "이메일 정보를 확인할 수 없습니다.", null));
+        }
         User user = userRepository.findByEmail(email)
-                .map(entity -> entity.update(name))
-                .orElse(User.builder()
-                        .email(email)
-                        .nickname(name)
-                        .build());
-        userRepository.save(user);
+                .orElseThrow(() -> new OAuth2AuthenticationException(
+                        new OAuth2Error("unauthorized_user", "승인되지 않은 사용자입니다.", null)));
+        if (name != null && !name.isBlank()) {
+            user.update(name);
+            userRepository.save(user);
+        }
     }
 }
